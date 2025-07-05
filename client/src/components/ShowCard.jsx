9daba9e20c93fedd16041d36249207f0dee351ee
bkee2 <bkee@seas.upenn.edu>
2025-07-05 12:39:04 -0400

    Added cast

--- client/src/components/ShowCard.jsx
+++ client/src/components/ShowCard.jsx
@@ -15,8 +15,8 @@ rounded-2xl hover:-translate-y-1 transition duration-300 w-full'>
             <p className='font-semibold mt-2 truncate'>{movie.title}</p>
 
             <p className='text-sm text-gray-400 mt-2'>
-                {new Date(movie.release_date).getFullYear()} ● {movie.genres.slice(0, 2).map
-                    (genre => genre.name).join(" | ")} ● {timeFormat(movie.runtime)}
+                {new Date(movie.release_date).getFullYear()} • {movie.genres.slice(0, 2).map
+                    (genre => genre.name).join(" | ")} • {timeFormat(movie.runtime)}
             </p>
 
             <div className='flex items-center justify-between mt-4 pb-3'>
